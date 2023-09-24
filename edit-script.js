fb92a299bd54853af3c27b948a4abae20d100f69
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2023-09-24 23:29:13 +0300

    Html-Css-JavaScript-Json
    Html-Css-JavaScript-Json kullanarak portfolyo hazırlama

---
+++ edit-script.js
@@ -0,0 +1,60 @@
+getData();
+function getData(){
+    fetch("./data.json")
+    .then(res=> res.json())
+    .then(data=> {
+        setAboutInformationToElement(data.aboutMe);
+        setNavigationToElement(data.navigations);
+        setFormElementByData(data.aboutMe);
+    });
+}
+
+let aboutMe = {};
+
+
+function setAboutInformationToElement(aboutMe) {    
+    const navNameElement = document.querySelector("nav div h1");    
+    const avatarElement = document.querySelector("nav div img");
+   
+    navNameElement.innerText = aboutMe.name;   
+    avatarElement.src = aboutMe.avatar;
+}
+
+
+function setNavigationToElement(navigations){
+    document.querySelector("nav ul").innerHTML = "";
+    for(const nav of navigations){
+        document.querySelector("nav ul").innerHTML += 
+        `<li>
+            <a href="${nav.path}">
+                <i class="${nav.icon}"></i>
+                ${nav.name}
+            </a>               
+        </li>`
+    }
+}
+
+function setFormElementByData(parameter){
+    aboutMe = parameter;
+    const nameEl = document.getElementById("name");
+
+    nameEl.value = aboutMe.name;
+}
+
+async function save(event) {
+   
+    event.preventDefault();
+    
+    const nameEl = document.getElementById("name");
+      
+    aboutMe.name = nameEl.value;
+
+    console.log(aboutMe);
+    await fetch("http://localhost:3000/aboutMe", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(aboutMe)
+    });
+}
